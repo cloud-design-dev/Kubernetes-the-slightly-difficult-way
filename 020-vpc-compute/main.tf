ca52f90e18962b03900a9b1c5cf3394c573ac329
greyhoundforty <ryantiffany@fastmail.com>
2023-10-24 15:38:59 -0500

    Initial commit for switch to private DNS

--- 020-vpc-compute/main.tf
+++ 020-vpc-compute/main.tf
@@ -122,45 +122,6 @@ resource "ibm_is_vpc_routing_table_route" "pod_cidrs" {
   next_hop      = ibm_is_instance.workers[count.index].primary_network_interface[0].primary_ipv4_address
 }
 
-resource "ibm_is_lb" "apiserver" {
-  name           = "${var.basename}-apiserver-lb"
-  subnets        = [data.terraform_remote_state.vpc.outputs.subnet_id]
-  profile        = "network-fixed"
-  type           = "public"
-  resource_group = data.terraform_remote_state.vpc.outputs.resource_group_id
-  tags           = concat(local.tags, ["zone:${local.vpc_zones[0].zone}"])
-}
-
-resource "ibm_is_lb_pool" "apiserver_pool" {
-  depends_on         = [ibm_is_lb.apiserver]
-  lb                 = ibm_is_lb.apiserver.id
-  name               = "${var.basename}-apiserver-pool"
-  protocol           = "tcp"
-  algorithm          = "round_robin"
-  health_delay       = "5"
-  health_retries     = "2"
-  health_timeout     = "2"
-  health_type        = "tcp"
-  health_monitor_port = "6443"
-  health_monitor_url = "/healthz"
-}
-
-resource "ibm_is_lb_listener" "apiserver_listener" {
-  depends_on = [ibm_is_lb_pool.apiserver_pool]
-  lb         = ibm_is_lb.apiserver.id
-  port       = "6443"
-  protocol   = "tcp"
-}
-
-resource "ibm_is_lb_pool_member" "apiserver_pool_member" {
-  count     = 3
-  lb        = ibm_is_lb.apiserver.id
-  pool      = element(split("/", ibm_is_lb_pool.apiserver_pool.id), 1)
-  port      = 6443
-  target_id = ibm_is_instance.controllers[count.index].id
-  weight    = 60
-}
-
 resource "local_file" "worker_csr" {
   count    = 3
   content  = <<EOF
@@ -193,44 +154,95 @@ module "ansible_inventory" {
 }
 
 
-# resource "ibm_resource_instance" "project_instance" {
-#   name              = "${var.basename}-dns-instance"
-#   resource_group_id = data.terraform_remote_state.vpc.outputs.resource_group_id
-#   location          = "global"
-#   service           = "dns-svcs"
-#   plan              = "standard-dns"
-# }
+resource "ibm_resource_instance" "project_instance" {
+  name              = "${var.basename}-dns-instance"
+  resource_group_id = data.terraform_remote_state.vpc.outputs.resource_group_id
+  location          = "global"
+  service           = "dns-svcs"
+  plan              = "standard-dns"
+}
 
-# resource "ibm_dns_zone" "zone" {
-#   name        = "gh40.local"
-#   instance_id = ibm_resource_instance.project_instance.guid
-#   description = "Private DNS Zone for VPC K8s cluster."
-#   label       = "testlabel"
-# }
+resource "ibm_dns_zone" "kubernetes" {
+  name        = "gh40.lab"
+  instance_id = ibm_resource_instance.project_instance.guid
+  description = "Private DNS Zone for VPC K8s cluster."
+  label       = "k8sthehardway"
+}
 
-# resource "ibm_dns_permitted_network" "permitted_network" {
-#   instance_id = ibm_resource_instance.project_instance.guid
-#   zone_id     = ibm_dns_zone.zone.zone_id
-#   vpc_crn     = data.terraform_remote_state.vpc.outputs.vpc_crn
-#   type        = "vpc"
-# }
+resource "ibm_dns_permitted_network" "permitted_network" {
+  instance_id = ibm_resource_instance.project_instance.guid
+  zone_id     = ibm_dns_zone.kubernetes.zone_id
+  vpc_crn     = data.terraform_remote_state.vpc.outputs.vpc_crn
+  type        = "vpc"
+}
 
-# resource "ibm_dns_resource_record" "controllers" {
-#   count       = 3
-#   instance_id = ibm_resource_instance.project_instance.guid
-#   zone_id     = ibm_dns_zone.zone.zone_id
-#   type        = "A"
-#   name        = "controller-${count.index}"
-#   rdata       = ibm_is_instance.controllers[count.index].primary_network_interface[0].primary_ipv4_address
-#   ttl         = 3600
-# }
+resource "ibm_dns_resource_record" "controllers" {
+  count       = 3
+  instance_id = ibm_resource_instance.project_instance.guid
+  zone_id     = ibm_dns_zone.kubernetes.zone_id
+  type        = "A"
+  name        = "controller-${count.index}"
+  rdata       = ibm_is_instance.controllers[count.index].primary_network_interface[0].primary_ipv4_address
+  ttl         = 3600
+}
 
-# resource "ibm_dns_resource_record" "workers" {
-#   count       = 3
-#   instance_id = ibm_resource_instance.project_instance.guid
-#   zone_id     = ibm_dns_zone.zone.zone_id
-#   type        = "A"
-#   name        = "worker-${count.index}"
-#   rdata       = ibm_is_instance.workers[count.index].primary_network_interface[0].primary_ipv4_address
-#   ttl         = 3600
-# }
+resource "ibm_dns_resource_record" "workers" {
+  count       = 3
+  instance_id = ibm_resource_instance.project_instance.guid
+  zone_id     = ibm_dns_zone.kubernetes.zone_id
+  type        = "A"
+  name        = "worker-${count.index}"
+  rdata       = ibm_is_instance.workers[count.index].primary_network_interface[0].primary_ipv4_address
+  ttl         = 3600
+}
+
+resource "ibm_dns_glb_monitor" "gslb_healthcheck" {
+  depends_on     = [ibm_dns_zone.kubernetes]
+  name           = "${var.basename}-glb-monitor"
+  instance_id    = ibm_resource_instance.project_instance.guid
+  description    = "Private DNS health check for kubes apiservers"
+  interval       = 60
+  retries        = 3
+  timeout        = 5
+  port           = 6443
+  type           = "TCP"
+}
+
+resource "ibm_dns_glb_pool" "k8s_pdns_glb_pool" {
+  depends_on                = [ibm_dns_zone.kubernetes]
+  name                      = "${var.basename}-glb-pool"
+  instance_id               = ibm_resource_instance.project_instance.guid
+  enabled                   = true
+  healthy_origins_threshold = 1
+  origins {
+    name        = "controller-1"
+    address     = ibm_is_instance.controllers[0].primary_network_interface[0].primary_ipv4_address
+    enabled     = true
+  }
+  origins {
+    name        = "controller-2"
+    address     = ibm_is_instance.controllers[0].primary_network_interface[0].primary_ipv4_address
+    enabled     = true
+  }
+    origins {
+    name        = "controller-3"
+    address     = ibm_is_instance.controllers[0].primary_network_interface[0].primary_ipv4_address
+    enabled     = true
+  }
+  monitor              = ibm_dns_glb_monitor.gslb_healthcheck.monitor_id
+  healthcheck_region   = var.region
+  healthcheck_subnets  = [data.terraform_remote_state.vpc.outputs.subnet_id]
+}
+
+
+resource "ibm_dns_glb" "k8s_pdns_glb" {
+  depends_on    = [ibm_dns_glb_pool.k8s_pdns_glb_pool]
+  name          = "${var.basename}-glb"
+  instance_id   = ibm_resource_instance.project_instance.guid
+  zone_id       = ibm_dns_zone.kubernetes.zone_id
+  ttl           = 120
+  enabled       = true
+  fallback_pool = ibm_dns_glb_pool.k8s_pdns_glb_pool.pool_id
+  default_pools = [ibm_dns_glb_pool.k8s_pdns_glb_pool.pool_id]
+}
+
